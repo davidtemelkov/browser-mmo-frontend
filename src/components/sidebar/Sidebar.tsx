45ffc7efcb834e3fafe13cac162faa8774a69e1d
davidtemelkov <dntemelkov@gmail.com>
2023-11-30 12:09:36 +0200

    Tweaked sidebar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,25 +1,77 @@
 import { FC } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import {
-  getEmailFromStorage,
-  getUserFromStorage,
-} from "../../utils/localStorage";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { getUserFromStorage } from "../../utils/localStorage";
 
 export const Sidebar: FC = () => {
-  const neededToLoadLinksProperlyIGuess = useNavigate();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const isSelected = (path: string): boolean => {
+    return location.pathname.includes(path);
+  };
 
   return (
-    <ul className="flex flex-col gap-3 items-center">
+    <div>
       {getUserFromStorage() && (
-        <li className="border-2 rounded-md w-[50%] text-center bg-blue-200">
-          <Link to={`/profile?email=${getEmailFromStorage()}`}>Profile</Link>
-        </li>
+        <ul className="flex flex-col gap-4 items-center">
+          <li
+            className={`border-2 rounded-md w-[50%] text-center ${
+              isSelected("/profile") ? "bg-green-300" : "bg-blue-300"
+            }`}
+          >
+            <Link to={`/profile`}>Profile</Link>
+          </li>
+          <li
+            className={`border-2 rounded-md w-[50%] text-center ${
+              isSelected("/ranking") ? "bg-green-300" : "bg-blue-300"
+            }`}
+          >
+            <Link to={`/ranking`}>Ranking</Link>
+          </li>
+          <li
+            className={`border-2 rounded-md w-[50%] text-center ${
+              isSelected("/quests") ? "bg-green-300" : "bg-blue-300"
+            }`}
+          >
+            <Link to={`/quests`}>Quests</Link>
+          </li>
+          <li
+            className={`border-2 rounded-md w-[50%] text-center ${
+              isSelected("/work") ? "bg-green-300" : "bg-blue-300"
+            }`}
+          >
+            <Link to={`/work`}>Work</Link>
+          </li>
+          <li
+            className={`border-2 rounded-md w-[50%] text-center ${
+              isSelected("/weapons") ? "bg-green-300" : "bg-blue-300"
+            }`}
+          >
+            <Link to={`/weapons`}>Weapons</Link>
+          </li>
+          <li
+            className={`border-2 rounded-md w-[50%] text-center ${
+              isSelected("/magic") ? "bg-green-300" : "bg-blue-300"
+            }`}
+          >
+            <Link to={`/magic`}>Magic</Link>
+          </li>
+          <li
+            className={`border-2 rounded-md w-[50%] text-center ${
+              isSelected("/mounts") ? "bg-green-300" : "bg-blue-300"
+            }`}
+          >
+            <Link to={`/mounts`}>Mounts</Link>
+          </li>
+          <li
+            className={`border-2 rounded-md w-[50%] text-center ${
+              isSelected("/dungeon") ? "bg-green-300" : "bg-blue-300"
+            }`}
+          >
+            <Link to={`/dungeon`}>Dungeon</Link>
+          </li>
+        </ul>
       )}
-      {getUserFromStorage() && (
-        <li className="border-2 rounded-md w-[50%] text-center bg-blue-200">
-          <Link to={`/quests`}>Quests</Link>
-        </li>
-      )}
-    </ul>
+    </div>
   );
 };
